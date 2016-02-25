2f90974808f37a96eb516ae72be4ff2ea063b081
David Boesen <davidboesen@davids-mac.local>
2016-02-25 11:40:16 -0800

    Initial Commit

---
+++ Calc It/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Calc It
+//
+//  Created by David Boesen on 2/25/16.
+//  Copyright © 2016 David Boesen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
